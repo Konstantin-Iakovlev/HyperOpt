3fe9339dd719c7c337e083fecc171e6e348d0681
Константин Яковлев <konstantinakovlev@192.168.0.12>
2024-07-11 11:48:04 +0300

    Switched to 3-layer CNN

--- datadistil/model.py
+++ datadistil/model.py
@@ -1,28 +1,32 @@
-import jax
 import haiku as hk
+import jax
 
 
 class CNN(hk.Module):
     def __init__(self, num_classes=10):
         super().__init__(name="CNN")
-        self.conv1 = hk.Conv2D(output_channels=6, kernel_shape=5, with_bias=True, padding='SAME')
-        self.conv2 = hk.Conv2D(output_channels=16, kernel_shape=5, with_bias=True, padding='VALID')
+        self.conv1 = hk.Conv2D(output_channels=8, kernel_shape=3)
+        self.conv2 = hk.Conv2D(output_channels=8, kernel_shape=3)
+        self.conv3 = hk.Conv2D(output_channels=8, kernel_shape=3)
         self.flatten = hk.Flatten()
-        self.linear1 = hk.Linear(120)
-        self.linear2 = hk.Linear(84)
-        self.linear3 = hk.Linear(num_classes)
+        self.logits = hk.Linear(num_classes, name='logits')
 
-    def __call__(self, x_batch, is_training):
+    def __call__(self, x_batch, is_training=True):
         x = self.conv1(x_batch)
-        x = jax.nn.leaky_relu(x)
-        x = hk.max_pool(x, window_shape=2, strides=2, padding='VALID')
+        x = hk.BatchNorm(False, False, 0.9)(x, is_training)
+        x = jax.nn.relu(x)
+        x = hk.avg_pool(x, window_shape=2, strides=2, padding='VALID')
         
         x = self.conv2(x)
-        x = jax.nn.leaky_relu(x)
-        x = hk.max_pool(x, window_shape=2, strides=2, padding='VALID')
+        x = hk.BatchNorm(False, False, 0.9)(x, is_training)
+        x = jax.nn.relu(x)
+        x = hk.avg_pool(x, window_shape=2, strides=2, padding='VALID')
 
+        x = self.conv3(x)
+        x = hk.BatchNorm(False, False, 0.9)(x, is_training)
+        x = jax.nn.relu(x)
+        x = hk.avg_pool(x, window_shape=2, strides=2, padding='VALID')
+        
         x = self.flatten(x)
-        x = jax.nn.leaky_relu(self.linear1(x))
-        x = jax.nn.leaky_relu(self.linear2(x))
-        x = self.linear3(x)
+        x = self.logits(x)
         return x
