b0893c7fd277d62b48cb9a09c016d3352e0d9c65
Константин Яковлев <konstantinakovlev@Konstantins-MacBook-Pro-2.local>
2024-07-11 08:48:03 +0300

    Switched to LeNet

--- datadistil/model.py
+++ datadistil/model.py
@@ -3,28 +3,26 @@ import haiku as hk
 
 
 class CNN(hk.Module):
-    def __init__(self, num_classes=10, channels=8):
+    def __init__(self, num_classes=10):
         super().__init__(name="CNN")
-        self.bn_aux = hk.BatchNorm(True, True, 0.9) # not used
-        self.conv1 = hk.Conv2D(output_channels=channels, kernel_shape=(5,5), with_bias=True, padding='SAME')
-        self.conv2 = hk.Conv2D(output_channels=channels, kernel_shape=(5,5), with_bias=True, padding='SAME')
-        self.conv3 = hk.Conv2D(output_channels=channels, kernel_shape=(5,5), with_bias=True, padding='SAME')
+        self.conv1 = hk.Conv2D(output_channels=6, kernel_shape=5, with_bias=True, padding='SAME')
+        self.conv2 = hk.Conv2D(output_channels=16, kernel_shape=5, with_bias=True, padding='VALID')
         self.flatten = hk.Flatten()
-        self.linear2 = hk.Linear(num_classes)
+        self.linear1 = hk.Linear(120)
+        self.linear2 = hk.Linear(84)
+        self.linear3 = hk.Linear(num_classes)
 
     def __call__(self, x_batch, is_training):
         x = self.conv1(x_batch)
         x = jax.nn.leaky_relu(x)
-        x = hk.max_pool(x, window_shape=(2, 2), strides=(2, 2), padding='VALID')
+        x = hk.max_pool(x, window_shape=2, strides=2, padding='VALID')
         
         x = self.conv2(x)
         x = jax.nn.leaky_relu(x)
-        x = hk.max_pool(x, window_shape=(2, 2), strides=(2, 2), padding='VALID')
+        x = hk.max_pool(x, window_shape=2, strides=2, padding='VALID')
 
-        x = self.conv3(x)
-        x = jax.nn.leaky_relu(x)
-        x = hk.max_pool(x, window_shape=(2, 2), strides=(2, 2), padding='VALID')
-        
         x = self.flatten(x)
-        x = self.linear2(x)
+        x = jax.nn.leaky_relu(self.linear1(x))
+        x = jax.nn.leaky_relu(self.linear2(x))
+        x = self.linear3(x)
         return x
