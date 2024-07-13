1ae4b8aeab4aa403b01769cda982bc1a0850f7bd
Константин Яковлев <konstantinakovlev@Konstantins-MacBook-Pro-2.local>
2024-07-13 10:58:48 +0300

    Inserted 2 warp layers

--- meta_learning/model.py
+++ meta_learning/model.py
@@ -1,16 +1,35 @@
+from typing import Any
 import haiku as hk
 import jax
+
+
+class WarpLayer(hk.Module):
+    def __init__(self, name=None):
+        super().__init__(name)
+        self.conv1 = hk.Conv2D(output_channels=32, kernel_shape=3, with_bias=False)
+        self.conv2 = hk.Conv2D(output_channels=32, kernel_shape=3, with_bias=False)
+    
+    def __call__(self, x_batch, is_training=True):
+        x = self.conv1(x_batch)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training)
+        x = jax.nn.relu(x)
+
+        x = self.conv2(x)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training)
+        x = jax.nn.relu(x)
+
+        return x
 
 
 class CNN(hk.Module):
     def __init__(self, num_classes=10):
         super().__init__(name="CNN")
-        self.conv1 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False)
-        self.warp1 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False, name='warp1')
-        self.conv2 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False)
-        self.warp2 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False, name='warp2')
-        self.conv3 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False)
-        self.warp3 = hk.Conv2D(output_channels=16, kernel_shape=3, with_bias=False, name='warp3')
+        self.conv1 = hk.Conv2D(output_channels=32, kernel_shape=3, with_bias=False)
+        self.warp1 = WarpLayer(name='warp1')
+        self.conv2 = hk.Conv2D(output_channels=32, kernel_shape=3, with_bias=False)
+        self.warp2 = WarpLayer(name='warp2')
+        self.conv3 = hk.Conv2D(output_channels=32, kernel_shape=3, with_bias=False)
+        self.warp3 = WarpLayer(name='warp3')
         self.flatten = hk.Flatten()
         self.logits = hk.Linear(num_classes, name='logits')
 
@@ -21,7 +40,6 @@ class CNN(hk.Module):
         x = jax.nn.relu(x)
 
         x = self.warp1(x)
-        x = hk.BatchNorm(True, True, 0.9)(x, is_training)
         
         x = self.conv2(x)
         x = hk.BatchNorm(True, True, 0.9)(x, is_training)
@@ -29,7 +47,6 @@ class CNN(hk.Module):
         x = jax.nn.relu(x)
 
         x = self.warp2(x)
-        x = hk.BatchNorm(True, True, 0.9)(x, is_training)
 
         x = self.conv3(x)
         x = hk.BatchNorm(True, True, 0.9)(x, is_training)
@@ -37,7 +54,6 @@ class CNN(hk.Module):
         x = jax.nn.relu(x)
 
         x = self.warp3(x)
-        x = hk.BatchNorm(True, True, 0.9)(x, is_training)
         
         x = self.flatten(x)
         x = self.logits(x)
