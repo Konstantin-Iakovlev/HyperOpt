73e12a3267ef28162b81e7b83ffa7f9de548dd7e
Константин Яковлев <konstantinakovlev@Konstantins-MacBook-Pro-2.local>
2024-07-24 10:57:00 +0300

    Fixed omniglot hyperparams

--- meta_learning/train.sh
+++ meta_learning/train.sh
@@ -12,11 +12,11 @@ do
     --backbone CNN \
     --dataset omniglot \
     --method ${method} \
-    --num_ways 2 --num_shots 80 \
+    --num_ways 5 --num_shots 1 \
     --val_ratio 0.2 \
-    --T 10 --outer_steps 501 \
+    --T 10 --outer_steps 1001 \
     --meta_batch_size 1 \
-    --outer_lr 1e-4 --val_freq 100 \
-    --inner_lr 1e-2 \
+    --outer_lr 1e-3 --val_freq 100 \
+    --inner_lr 1e-1 \
     --batch_size 16
 done
