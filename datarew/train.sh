22ef06b76f50aac452c9610f754767e3b1f7ca69
Константин Яковлев <konstantinakovlev@Konstantins-MacBook-Pro-2.local>
2024-08-04 20:03:06 +0300

    Fixed hyperparams

--- datarew/train.sh
+++ datarew/train.sh
@@ -1,7 +1,7 @@
 method=baseline
 # method=proposed_0.99
 # method=proposed_0.9
-# method=IFT_3
+# method=IFT_2
 # method=luketina
 
 #export XLA_FLAGS="--xla_gpu_strict_conv_algorithm_picker=false"
@@ -10,7 +10,7 @@ for seed in 0 1 2 3 4
 do
     python train.py --seed ${seed} --imb_fact 50 \
     --method ${method} \
-    --T 10 --outer_steps 1001 --dataset cifar10 --data_size 1_000_000 \
-    --outer_lr 1e-1 --wnet_hidden 100 --val_freq 100 --backbone WideResNet \
-    --inner_lr 1e-2
+    --T 5 --outer_steps 2001 --dataset cifar10 --data_size 1_000_000 \
+    --outer_lr 1e-5 --wnet_hidden 100 --val_freq 500 --backbone ResNet18 \
+    --inner_lr 1e-1 --batch_size 128
 done
