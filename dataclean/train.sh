7bf3a329a3bf907ef16a0bd5574d0cc35a96739d
Константин Яковлев <konstantinakovlev@Konstantins-MacBook-Pro-2.local>
2024-08-07 22:46:55 +0300

    Fixed training args

--- dataclean/train.sh
+++ dataclean/train.sh
@@ -1,8 +1,7 @@
-# method=baseline
-# method=proposed_0.99
-method=proposed_0.9
-# method=IFT_3
-# method=luketina
+method=baseline
+#method=proposed_0.9
+#method=IFT_5
+#method=luketina
 
 #export XLA_FLAGS="--xla_gpu_strict_conv_algorithm_picker=false"
 
@@ -10,7 +9,7 @@ for seed in 0 1 2 3 4
 do
     python train.py --seed ${seed} --corruption 0.3 \
     --method ${method} \
-    --T 5 --outer_steps 301 --dataset cifar10 --data_size 2000 \
-    --outer_lr 1e-1 --val_freq 10 --backbone ResNet18 \
-    --inner_lr 1e-2 --batch_size 2000
+    --T 10 --outer_steps 201 --dataset fmnist --data_size 1000 \
+    --outer_lr 1e-1 --val_freq 50 --backbone CNN \
+    --inner_lr 1e-1 --batch_size 1000
 done
